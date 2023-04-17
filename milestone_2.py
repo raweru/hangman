16bb6d04b73d59cdf3f277014de577c757684c8a
Dominik Dekleva <dominik.dekleva@hotmail.com>
2023-04-17 18:41:16 +0100

    Task 4: Check that the input is a single character, isalpha func

--- milestone_2.py
+++ milestone_2.py
@@ -1,12 +1,11 @@
 import random
-import string
 
 word_list = ["mango", "apple", "peach", "pear", "strawberry"]
 word = random.choice(word_list)
 print(word)
 
 guess = input("Enter a single letter: ")
-if len(guess) == 1 and guess in string.ascii_letters:
+if len(guess) == 1 and guess.isalpha() == True:
     print("Good guess!")
 else:
     print("Oops! That is not a valid input.")
